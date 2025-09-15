6b4a6311d47b3abca7f488ca4428d565963b2d4b
sujan07 <sujanpokharel704@gmail.com>
2025-09-15 17:01:06 +0545

    update the migration into postgres

--- prisma/migrations/20250902013241_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `booking` ADD COLUMN `username` VARCHAR(191) NOT NULL DEFAULT '';
--- prisma/migrations/20250906101619_init/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `resetUpdateOtp` on the `user` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `user` DROP COLUMN `resetUpdateOtp`,
-    ADD COLUMN `UpdateOtpExpire` DATETIME(3) NULL;
