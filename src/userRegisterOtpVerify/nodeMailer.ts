cddd7acb6429995070e47a4fa64e6fdca5ae5b22
sujan07 <155717764+SUJANPOKHAREL07@users.noreply.github.com>
2025-10-29 17:26:19 +0545

    Update nodeMailer.ts port

--- src/userRegisterOtpVerify/nodeMailer.ts
+++ src/userRegisterOtpVerify/nodeMailer.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: 587,
+  port: 465,
   secure: false, // use TLS
   auth: {
     user: process.env.SMTP_USER,
